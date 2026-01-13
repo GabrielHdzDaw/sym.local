cc4d4552f8ecaa6cd882cf831272f073b3245f9c
GabrielHdzDaw <gabrielhernandezdaw@gmail.com>
2026-01-13 23:51:55 +0100

    arregladas comillas en el nav

--- templates/navegacion.php
+++ templates/navegacion.php
@@ -21,32 +21,32 @@ use App\Utils\Utils;
             <ul class="nav navbar-nav">
                 <?php if (Utils::esOpcionMenuActiva('/') == true )
                     echo '<li class="active lien">';
-                else echo '<li class=”lien”>'; ?>
+                else echo '<li class="lien">'; ?>
                 <a href="/"><i class="fa fa-home sr-icons"></i> Home</a></li>
 
                 <?php if (Utils::esOpcionMenuActiva('/galeria') == true )
                     echo '<li class="active lien">';
-                else echo '<li class=”lien”>'; ?>
+                else echo '<li class="lien">'; ?>
                 <a href="/galeria"><i class="fa fa-home sr-icons"></i> Galería</a></li>
 
                 <?php if (Utils::esOpcionMenuActiva('/asociados') == true )
                     echo '<li class="active lien">';
-                else echo '<li class=”lien”>'; ?>
+                else echo '<li class="lien">'; ?>
                 <a href="/asociados"><i class="fa fa-home sr-icons"></i> Asociados</a></li>
 
                 <?php if (Utils::esOpcionMenuActiva('/about') == true )
                     echo '<li class="active lien">';
-                else echo '<li class=”lien”>'; ?>
+                else echo '<li class="lien">'; ?>
                 <a href="/about"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
 
                 <?php if (Utils::esOpcionMenuActiva('/blog') == true )
                     echo '<li class="active lien">';
-                else echo '<li class=”lien”>'; ?>
+                else echo '<li class="lien">'; ?>
                 <a href="/blog"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
 
                 <?php if (Utils::esOpcionMenuActiva('/contact') == true )
                     echo '<li class="active lien">';
-                else echo '<li class=”lien”>'; ?>
+                else echo '<li class="lien">'; ?>
                 <a href="/contact"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
             </ul>
         </div>
