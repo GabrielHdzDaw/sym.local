a30a66101a4097dd46fb32f3aa05ab8dfe5e9de9
= <=>
2026-01-14 20:34:03 +0100

    5.2 completado

--- migrations/Version20260114191514.php
+++ migrations/Version20260114191514.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260114085559 extends AbstractMigration
+final class Version20260114191514 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,8 @@ final class Version20260114085559 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE categoria (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('ALTER TABLE imagen ADD categoria_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE asociado CHANGE nombre nombre VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE imagen ADD categoria_id INT DEFAULT NULL, CHANGE nombre nombre VARCHAR(255) NOT NULL, CHANGE descripcion descripcion VARCHAR(200) DEFAULT NULL, CHANGE num_downloads num_downloads INT DEFAULT NULL');
         $this->addSql('ALTER TABLE imagen ADD CONSTRAINT FK_8319D2B33397707A FOREIGN KEY (categoria_id) REFERENCES categoria (id)');
         $this->addSql('CREATE INDEX IDX_8319D2B33397707A ON imagen (categoria_id)');
     }
@@ -29,9 +29,9 @@ final class Version20260114085559 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE categoria');
+        $this->addSql('ALTER TABLE asociado CHANGE nombre nombre VARCHAR(100) NOT NULL');
         $this->addSql('ALTER TABLE imagen DROP FOREIGN KEY FK_8319D2B33397707A');
         $this->addSql('DROP INDEX IDX_8319D2B33397707A ON imagen');
-        $this->addSql('ALTER TABLE imagen DROP categoria_id');
+        $this->addSql('ALTER TABLE imagen DROP categoria_id, CHANGE nombre nombre VARCHAR(50) NOT NULL, CHANGE descripcion descripcion VARCHAR(100) NOT NULL, CHANGE num_downloads num_downloads INT NOT NULL');
     }
 }
